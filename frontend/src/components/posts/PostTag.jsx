757b6fc9f960dac0189361c81cb735931867ac18
unknown <a99701841@gmail.com>
2026-05-13 19:28:37 +0900

    tag

--- frontend/src/components/posts/PostTag.jsx
+++ frontend/src/components/posts/PostTag.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { getTagColor } from '@/hooks/useTagColor'
-const PostTag = ({ tag ,onClick}) => {
+import React from "react"
+import { getTagColor } from "@/hooks/useTagColor"
+const PostTag = ({ tag, onClick, showDelete = true }) => {
   return (
-    <span className='post-tag'
-    style={{backgroundColor:getTagColor(tag)}}
-    >
-      <span>
-
-        {tag}
-      </span>
-      <button 
-      className='post-tag-delete' 
-      onClick={(e)=>{
-        e.preventDefault()
-        e.stopPropagation()
-        onClick?.()
-        }}>X</button>
+    <span className="post-tag" style={{ backgroundColor: getTagColor(tag) }}>
+      <span>{tag}</span>
+      {showDelete && (
+        <button
+          className="post-tag-delete"
+          onClick={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            onClick?.()
+          }}
+        >
+          X
+        </button>
+      )}
     </span>
   )
 }
